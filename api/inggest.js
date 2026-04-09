b06f33c1f75019adeb30a1fc62ac85e70e126298
goutham <gouthamr977@gmail.com>
2026-04-09 20:01:01 +0530

    four

--- api/inggest.js
+++ api/inggest.js
@@ -1,10 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "../server/Inngest/index.js";
 import { functions } from "../server/Inngest/index.js";
-import connectDB from "../server/configs/db.js";
 
-// connect DB before handling
-await connectDB();
+console.log("KEY:", process.env.INNGEST_SIGNING_KEY); // 👈 ADD HERE
 
 export default serve({
   client: inngest,
