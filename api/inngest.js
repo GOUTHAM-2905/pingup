dc1370263d39c45323b9fc20bab7c4efe3c19d09
goutham <gouthamr977@gmail.com>
2026-04-10 00:09:14 +0530

    error

--- api/inngest.js
+++ api/inngest.js
@@ -1,18 +1,9 @@
-import express from "express";
 import { serve } from "inngest/express";
 import { inngest } from "../server/inngest/index.js";
 import { functions } from "../server/inngest/index.js";
 
-const app = express();
-
-app.use(express.json());
-
-app.all(
-  "/",
-  serve({
-    client: inngest,
-    functions,
-  })
-);
-
-export default app;
+// ✅ NO EXPRESS NEEDED
+export default serve({
+  client: inngest,
+  functions,
+});
